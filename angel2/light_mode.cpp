08745d421d6a86f41aac16b597946af071a5d0b1
Andrew D Benson <drew@adbenson.net>
2017-05-07 13:11:11 -0400

    RGBW generate and parse

--- angel2/light_mode.cpp
+++ angel2/light_mode.cpp
@@ -7,7 +7,11 @@ LightMode::LightMode(int maxPixels) {
 }
 
 uint32_t LightMode::rgb(uint8_t r, uint8_t g, uint8_t b) {
-  return ((uint32_t)r << 16) | ((uint32_t)g <<  8) | b;
+  return ((uint32_t)r << 16) | ((uint32_t)g << 8) | b;
+}
+
+uint32_t LightMode::rgbw(uint8_t r, uint8_t g, uint8_t b, uint8_t w) {
+  return ((uint32_t)w << 24) | ((uint32_t)r << 16) | ((uint32_t)g <<  8) | b;
 }
 
 // Input a value 0 to 255 to get a color value.
@@ -25,4 +29,24 @@ uint32_t LightMode::hue(byte hue) {
     return rgb(hue * 3, 255 - hue * 3, 0);
   }
 }
+
+uint32_t LightMode::red(uint32_t color) {
+  return (color >> 16) & 255;
+}
+
+uint32_t LightMode::green(uint32_t color) {
+  return (color >> 8) & 255;
+}
+
+uint32_t LightMode::blue(uint32_t color) {
+  return color & 255;
+}
+
+uint32_t LightMode::white(uint32_t color) {
+  return (color >> 24) & 255;
+}
+
+
+
+
 
--- angel2/light_mode.h
+++ angel2/light_mode.h
@@ -13,7 +13,7 @@ class LightMode {
     virtual void display(Adafruit_NeoPixel* strip, int meta) = 0;
 
     static uint32_t rgb(uint8_t r, uint8_t g, uint8_t b);
-    
+    static uint32_t rgbw(uint8_t r, uint8_t g, uint8_t b, uint8_t w);    
     static uint32_t hue(byte hue);
 
   protected:
